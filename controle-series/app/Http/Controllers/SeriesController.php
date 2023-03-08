12e155aa95b8da47c7325930c020433a5789c6d0
Rodrigo Gomes <cklrod@hotmail.com>
2023-03-08 16:07:09 -0300

    criação do layout component e separação do index series

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -15,14 +15,7 @@ class SeriesController extends Controller
     {
         $series = ['Flash', 'Demolidor', 'O Justiceiro', 'Arqueiro Verde'];
 
-        $html = '<ul>';
-
-        foreach($series as $serie){
-            $html .= "<li>{$serie}</li>";
-        }
-        $html .= '</ul>';
-
-        return $html;
+        return view('series.index', ['series' => $series]);
     }
 
     /**
