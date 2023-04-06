a65664768eb310bde0c17d55c9ee98550e38da8b
Rodrigo Gomes <cklrod@hotmail.com>
2023-04-06 15:03:06 -0300

    rest

--- controle-series/app/Http/Controllers/Api/SeriesController.php
+++ controle-series/app/Http/Controllers/Api/SeriesController.php
@@ -3,13 +3,84 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
-    public function seriesList()
+
+    private $repository;
+
+    public function __construct(SeriesRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    public function index()
     {
         return Series::all();
+    }
+
+    public function store(SeriesFormRequest $request)
+    {
+        return response()
+            ->json($this->repository->add($request), 201);
+    }
+
+    public function show(int $series)
+    {
+        $series = Series::whereId($series)->with('seasons.episodes')->first();
+        return $series;
+    }
+
+    public function update(Series $series, SeriesFormRequest $request)
+    {
+        $series->fill($request->all());
+        $series->save();
+
+        return $series;
+    }
+
+    public function destroy(int $series)
+    {
+        $delete =  Series::destroy($series);
+        return $delete ? 'Série deletada' : 'Erro ao deletar série';
+    }
+
+    public function upload(Request $request)
+    {
+        // Verificar se o arquivo foi enviado na requisição
+        if (!$request->hasFile('cover')) {
+            return response()->json(['erro' => 'Arquivo não enviado'], 400);
+        }
+
+        // Obter o arquivo enviado na requisição
+        $arquivo = $request->file('cover');
+
+        // Obter o diretório base de armazenamento dos arquivos
+        $diretorioBase = storage_path('app/public/');
+
+        // Obter o parâmetro "path" da URL, ou definir um valor padrão
+        $path = $request->input('path', 'default');
+
+        // Criar o diretório para armazenar o arquivo, se ele não existir
+        $caminhoDiretorio = $diretorioBase . '/' . $path;
+        if (!file_exists($caminhoDiretorio)) {
+            mkdir($caminhoDiretorio, 0755, true);
+        }
+
+        // Definir o nome do arquivo
+        $nomeArquivo = time() . '.' . $arquivo->getClientOriginalExtension();
+
+        // Salvar o arquivo no diretório
+        $arquivo->move($caminhoDiretorio, $nomeArquivo);
+
+        // Gerar o caminho completo para o arquivo
+        $caminhoArquivo = $caminhoDiretorio . '/' . $nomeArquivo;
+
+        // Retornar a resposta da API
+        return response()->json(['caminho' => $caminhoArquivo]);
     }
 }
--- controle-series/routes/api.php
+++ controle-series/routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/series', [SeriesController::class, 'seriesList']);
+Route::apiResource('/series', SeriesController::class);
+Route::post('/series/upload', [SeriesController::class, 'upload']);
